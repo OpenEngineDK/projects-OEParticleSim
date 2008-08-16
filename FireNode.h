8c08db1f928f0b4eacb518bd9b5c55bd0238d0d2
salomonhippie <salomonhippie@gmail.com>
2008-08-16 14:43:01 +0000

    support new engine structure and new particle system structure

--- FireNode.h
+++ FireNode.h
@@ -1,35 +1,44 @@
 #ifndef _FIRE_NODE_H_
 #define _FIRE_NODE_H_
 
-#include <ParticleSystem/ParticleGroup.h>
+// base particle system
 #include <ParticleSystem/ParticleSystem.h>
+#include <ParticleSystem/ParticleCollection.h>
+#include <ParticleSystem/IParticleEffect.h>
 
-#include <ParticleSystem/StaticForceModifier.h>
-#include <ParticleSystem/RandomTimeBasedEmitterModifier.h>
-#include <ParticleSystem/ColorModifier.h>
-#include <ParticleSystem/SizeModifier.h>
-#include <ParticleSystem/LifespanModifier.h>
-#include <ParticleSystem/OrientationModifier.h>
-
-#include <ParticleSystem/IEmitter.h>
-#include <ParticleSystem/PointEmitter.h>
-
-#include <Geometry/Face.h>
-#include <Geometry/FaceSet.h>
-#include <Renderers/IRenderer.h>
-#include <Renderers/IRenderingView.h>
-#include <Renderers/IRenderNode.h>
-
+// particle types
 #include <ParticleSystem/Particles/IParticle.h>
 #include <ParticleSystem/Particles/Position.h>
+#include <ParticleSystem/Particles/PreviousPosition.h>
 #include <ParticleSystem/Particles/Orientation.h>
 #include <ParticleSystem/Particles/Lifespan.h>
 #include <ParticleSystem/Particles/Color.h>
 #include <ParticleSystem/Particles/Size.h>
+#include <ParticleSystem/Particles/Texture.h>
+
+// predefined modifiers
+#include <ParticleSystem/StaticForceModifier.h>
+#include <ParticleSystem/LinearColorModifier.h>
+#include <ParticleSystem/SizeModifier.h>
+#include <ParticleSystem/LifespanModifier.h>
+#include <ParticleSystem/VerletModifier.h>
+#include <ParticleSystem/RandomTextureRotationModifier.h>
+
+// predefined initializers
+#include <ParticleSystem/RandomTextureInitializer.h>
+#include <ParticleSystem/RandomLifespanInitializer.h>
+#include <ParticleSystem/RandomVerletInitializer.h>
+
+#include <Renderers/IRenderer.h>
+#include <Renderers/IRenderingView.h>
+#include <Renderers/IRenderNode.h>
+#include <Scene/ISceneNode.h>
 
 #include <Renderers/OpenGL/TextureLoader.h>
 #include <Resources/ITextureResource.h>
 #include <Resources/ResourceManager.h>
+
+#include <Meta/OpenGL.h>
 
 using namespace OpenEngine::Renderers;
 using namespace OpenEngine::Scene;
@@ -38,145 +47,216 @@ using namespace OpenEngine::Resources;
 using namespace OpenEngine::Renderers::OpenGL;
 
 
-#define TYPE Color < Size < Orientation < Position < Lifespan < IParticle > > > > >
+typedef Color < Texture <Size < Orientation < PreviousPosition < Position < Lifespan < IParticle > > > > > > > TYPE;
 
-class FireNode : public IRenderNode {
+class FireNode : public IRenderNode, public IParticleEffect {
 private:
-    ParticleGroup<TYPE>* particleGroup;
-    ITextureResourcePtr texr;
+    ParticleCollection<TYPE>* particles;
 
+    ParticleSystem* system;
+
+    //initializers
+    RandomVerletInitializer<TYPE> initverlet;
+    RandomTextureInitializer<TYPE> inittex;
+
+    //modifiers
+    VerletModifier<TYPE> verlet;
+    StaticForceModifier<TYPE> wind, antigravity;
+    SizeModifier<TYPE> sizemod;
+    LinearColorModifier<TYPE> colormod;
+    LifespanModifier<TYPE> lifemod;
+    RandomTextureRotationModifier<TYPE> rotationmod;
+
+    float percentChanceOfSpawning;
+    unsigned int spawns, spawnsVar; 
+        
 public:
-    FireNode() {
-        const unsigned int numberOfParticles = 1000;
-        particleGroup = new ParticleGroup<TYPE>(numberOfParticles);
-
-        // emitter
-        IEmitter* emitter = new PointEmitter(Vector<3,float>(0.1f));
-        particleGroup->AddModifier( new RandomTimeBasedEmitterModifier<TYPE>(10,50,emitter) );
-
-        // add updrift
-        StaticForceModifier<TYPE>* antigravity = 
-            new StaticForceModifier<TYPE>(Vector<3,float>(0,0.00682,0));
-        particleGroup->AddModifier( antigravity );
-
-        StaticForceModifier<TYPE>* wind = 
-            new StaticForceModifier<TYPE>(Vector<3,float>(0.00291,0,0));
-        particleGroup->AddModifier( wind );
-
-//         LifespanModifier<TYPE>* lifespan = 
-//             new LifespanModifier<TYPE>();
-//         particleGroup->AddModifier( lifespan );
-
-        OrientationModifier<TYPE>* orientation = 
-            new OrientationModifier<TYPE>();
-        particleGroup->AddModifier( orientation );
-        
-    
-        ColorModifier<TYPE>* colormodifier = 
-            new ColorModifier<TYPE>();
-        particleGroup->AddModifier( colormodifier );
-
-        SizeModifier<TYPE>* sizemodifier = 
-            new SizeModifier<TYPE>();
-        particleGroup->AddModifier( sizemodifier );
-
+    FireNode(ParticleSystem* system): system(system),
+                initverlet(Vector<3,float>(0.0,1.,0.0), PI),
+                wind(Vector<3,float>(1.591,0,0)),
+                antigravity(Vector<3,float>(0,0.382,0)),
+                percentChanceOfSpawning(0.6),
+                spawns(100), spawnsVar(2){
+        particles = system->CreateParticles<TYPE>(500);     
+ 
         //load texture resource
-        texr = ResourceManager<ITextureResource>::Create("Smoke/smoke01.tga");
-    }
+        ITextureResourcePtr texr1 = ResourceManager<ITextureResource>::Create("Smoke/smoke01.tga");
+        ITextureResourcePtr texr2 = ResourceManager<ITextureResource>::Create("Smoke/smoke02.tga");
+        ITextureResourcePtr texr3 = ResourceManager<ITextureResource>::Create("Smoke/smoke03.tga");
 
-    ~FireNode() {
-    }
+        inittex.AddTextureResource(texr1);
+        inittex.AddTextureResource(texr2);
+        inittex.AddTextureResource(texr3);
+}
 
-    IParticleGroup* GetParticleGroup() {
-        return particleGroup;
-    }
+~FireNode() {
+    delete[] particles;
+}
+ 
+void Handle(ParticleEventArg e) {
 
-    ISceneNode* GetSceneNode() {
-        return this;
-    }
+    //emitter code
+    float change = (rand()/(float)RAND_MAX);
+    if (percentChanceOfSpawning >= change)
+        for (unsigned int i = 0; 
+             i < spawns + (unsigned int)((rand()/(float)RAND_MAX)*spawnsVar); 
+             i++)
+            Emit();
+    
+//     for (particles->iterator.Reset(); 
+//          particles->iterator.HasNext(); 
+//          particles->iterator.Next()) {
+            
+//         //TYPE& particle = particles->iterator.Element();
+            
+//     }
 
-    void Apply(IRenderingView* view) {
+
+
+    for (particles->iterator.Reset(); 
+         particles->iterator.HasNext(); 
+         particles->iterator.Next()) {
+        TYPE& particle = particles->iterator.Element();
         
-        glPushAttrib(GL_LIGHTING);    
-        glDisable(GL_LIGHTING);
-        glDepthMask(GL_FALSE);
-        glEnable(GL_BLEND);
-        glEnable(GL_TEXTURE_2D);
-        glEnable(GL_COLOR_MATERIAL);
-        glBlendFunc(GL_ONE,GL_ONE_MINUS_SRC_ALPHA);
+        // custom modify particles
         
-        TYPE* particles = particleGroup->GetParticles();
-        //glBegin(GL_LINES);
-        for (unsigned int i = 0; i < particleGroup->GetNumberOfActiveParticles(); i++) {
-            
-            TYPE particle = particles[i];
-            
-            //Set texture
-            if (texr != NULL) {
-                if (texr->GetID() == 0) {
-                    TextureLoader::LoadTextureResource(texr);
-                    //logger.info << texr->GetID() << logger.end;
-                }
-                glBindTexture(GL_TEXTURE_2D, texr->GetID());
-            }
-            
-            else {
-                glBindTexture(GL_TEXTURE_2D, 0);
-            }
-            
-            glPushMatrix();
-            glTranslatef(particle.position[0], particle.position[1], particle.position[2]);
-            
-            // billboard
-            float modelview[16];
-            glGetFloatv(GL_MODELVIEW_MATRIX, modelview);
-            for( int i=0; i<3; i++ ) 
-                for( int j=0; j<3; j++ ) {
-                    if ( i==j )
-                        modelview[i*4+j] = 1.0;
-                    else
-                        modelview[i*4+j] = 0.0;
-                }
-            
-            glLoadMatrixf(modelview);
-            
-            glRotatef(particle.orientation[2], 0,0,1);
-            
-            float scale = particle.size;
-            glScalef(scale,scale,scale);
-            
-            // constant color
-            float c[4] = {.2,0.0,0.0,1.0};
-            
-            for(unsigned int i=0;i<4;i++)
-                c[i] = particle.color[i];
-            
-            glColor4fv(c);
-            
-            // constant size 
-            float s = 3.0;
-            
-            glBegin(GL_QUADS);
-            glTexCoord2f(0.0, 0.0);
-            glVertex3f(-1.0*s, -1.0*s, 0.0);
-            glTexCoord2f(0.0, 1.0);
-            glVertex3f(-1.0*s, s*1.0f, 0);
-            glTexCoord2f(1.0, 1.0);
-            glVertex3f(s*1, s*1, 0);
-            glTexCoord2f(1.0, 0.0);
-            glVertex3f(s*1, -1*s, 0);
-            
-            glEnd();
-            
-            glPopMatrix();
+        //random force in x-axis
+        float change = (rand()/(float)RAND_MAX);
+        if (0.7 >= change) {
+            Vector<3,float> maxdir (0.0,0.0,0.9);
+            particle.position += maxdir*((rand()/(float)RAND_MAX*2)-1);
         }
         
-        glDisable(GL_BLEND);
-        glPopAttrib();
-        
-        // render subnodes
-        VisitSubNodes(*view);      
+        // predefined particle modifiers
+        wind.Process(e.dt, particle);
+        antigravity.Process(e.dt, particle);
+        sizemod.Process(e.dt,particle);
+        verlet.Process(e.dt, particle);
+        colormod.Process(e.dt, particle);
+        rotationmod.Process(e.dt, particle);
+        lifemod.Process(e.dt, particles->iterator);
     }
+}
+
+    void inline Emit() {
+    if (particles->GetActiveParticles() >= particles->GetSize())
+        return;
+    //    logger.info << "num parts " << particles->GetActiveParticles() << logger.end;
+    //    logger.info << "emit" << logger.end;
+    TYPE& particle = particles->NewParticle();
+    // static pos
+    particle.position = Vector<3,float>(.0);
+        
+    // lifespan
+    float lifespanVar = 1000.0;
+    particle.lifespan = 1500.0 + (rand()/((float)RAND_MAX)-1)*lifespanVar;
+
+    initverlet.Process(particle);
+    inittex.Process(particle);
+    //size
+    particle.size = (rand()/((float)RAND_MAX)) * 0.5;
+
+    //color
+    particle.color = particle.startColor = Vector<4,float>(0.85,0.7,0.0,0.80);
+    particle.endColor = Vector<4,float>(0.825,0.00,0.0,0.03);
+
+    // random direction
+    float angle = 0.1*PI;
+    Vector<3,float> direction(0.0,1.0,0.0);
+    float r = (rand()/((float)RAND_MAX))*angle;
+    float p = (rand()/((float)RAND_MAX))*2*PI;
+    Quaternion<float> q(r,p, 0);
+    q.Normalize();
+    particle.previousPosition = particle.position - q.RotateVector(direction);
+
+}
+
+void Apply(IRenderingView* view) {
+        
+    glPushAttrib(GL_LIGHTING);    
+    glDisable(GL_LIGHTING);
+    glDepthMask(GL_FALSE);
+    glEnable(GL_BLEND);
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_COLOR_MATERIAL);
+    glBlendFunc(GL_ONE,GL_ONE_MINUS_SRC_ALPHA);
+        
+                
+    for (particles->iterator.Reset(); particles->iterator.HasNext(); particles->iterator.Next()) {
+        TYPE& particle = particles->iterator.Element();
+        ITextureResourcePtr texr = particle.texture;
+            
+        //Set texture
+        if (texr != NULL) {
+            if (texr->GetID() == 0) {
+                TextureLoader::LoadTextureResource(texr);
+                //logger.info << texr->GetID() << logger.end;
+            }
+            glBindTexture(GL_TEXTURE_2D, texr->GetID());
+        }
+            
+        else {
+            glBindTexture(GL_TEXTURE_2D, 0);
+        }
+            
+        glPushMatrix();
+        glTranslatef(particle.position[0], particle.position[1], particle.position[2]);
+            
+        // billboard
+        float modelview[16];
+        glGetFloatv(GL_MODELVIEW_MATRIX, modelview);
+        for( int i=0; i<3; i++ ) 
+            for( int j=0; j<3; j++ ) {
+                if ( i==j )
+                    modelview[i*4+j] = 1.0;
+                else
+                    modelview[i*4+j] = 0.0;
+            }
+            
+        glLoadMatrixf(modelview);
+            
+        glRotatef(particle.rotation, 0,0,1);
+            
+        float scale = particle.size;
+        glScalef(scale,scale,scale);
+            
+        // constant color
+        float c[4] = {.2,0.0,0.0,1.0};
+            
+        for(unsigned int i=0;i<4;i++)
+            c[i] = particle.color[i];
+            
+        glColor4fv(c);
+            
+        // constant size 
+        float s = 3.0;
+            
+        glBegin(GL_QUADS);
+        glTexCoord2f(0.0, 0.0);
+        glVertex3f(-1.0*s, -1.0*s, 0.0);
+        glTexCoord2f(0.0, 1.0);
+        glVertex3f(-1.0*s, s*1.0f, 0);
+        glTexCoord2f(1.0, 1.0);
+        glVertex3f(s*1, s*1, 0);
+        glTexCoord2f(1.0, 0.0);
+        glVertex3f(s*1, -1*s, 0);
+            
+        glEnd();
+            
+        glPopMatrix();
+    }
+        
+    glDisable(GL_BLEND);
+    glPopAttrib();
+        
+    // render subnodes
+    VisitSubNodes(*view);      
+}
+
+ISceneNode* GetSceneNode() {
+    return this;
+}
+
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Particle System Simulator demo.
+// Main setup for the OpenEngine Racer project.
 // -------------------------------------------------------------------
 // Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
 // 
@@ -7,24 +7,273 @@
 // See the GNU General Public License for more details (see LICENSE). 
 //--------------------------------------------------------------------
 
+// OpenEngine stuff
 #include <Meta/Config.h>
-#include <Core/GameEngine.h>
-#include <Core/IGameFactory.h>
+
+// Core structures
+#include <Core/Engine.h>
+
+// Display structures
+#include <Display/Frustum.h>
+#include <Display/ViewingVolume.h>
+#include <Display/Camera.h>
+#include <Display/IFrame.h>
+
+// SDL implementation
+#include <Display/SDLFrame.h>
+#include <Devices/SDLInput.h>
+
+// Particle System 
+#include <ParticleSystem/ParticleSystem.h>
+
+// OpenGL rendering implementation
+#include <Renderers/OpenGL/Renderer.h>
+#include <Renderers/OpenGL/RenderingView.h>
+#include <Renderers/OpenGL/TextureLoader.h>
+
+// Resources
+#include <Resources/File.h>
+#include <Resources/DirectoryManager.h>
+#include <Resources/ResourceManager.h>
+
+// OBJ and TGA plugins
+#include <Resources/TGAResource.h>
+#include <Resources/OBJResource.h>
+
+// Scene structures
+#include <Scene/SceneNode.h>
+
+// Utilities and logger
 #include <Logging/Logger.h>
 #include <Logging/StreamLogger.h>
-#include "GameFactory.h"
+#include <Utils/Statistics.h>
+#include <Utils/MoveHandler.h>
+#include <Utils/QuitHandler.h>
 
+// OEParticleSim utility files
+#include "FireNode.h"
+
+// Additional namespaces
 using namespace OpenEngine::Core;
 using namespace OpenEngine::Logging;
+using namespace OpenEngine::Devices;
+using namespace OpenEngine::Renderers::OpenGL;
+using namespace OpenEngine::Renderers;
+using namespace OpenEngine::Resources;
+using namespace OpenEngine::Utils;
+using namespace OpenEngine::Display;
+using namespace OpenEngine::ParticleSystem;
 
-int main( int argc, char** argv ) {
+// Configuration structure to pass around to the setup methods
+struct Config {
+    IEngine&              engine;
+    IFrame*               frame;
+    Viewport*             viewport;
+    IViewingVolume*       viewingvolume;
+    Camera*               camera;
+    Frustum*              frustum;
+    IRenderer*            renderer;
+    IMouse*               mouse;
+    IKeyboard*            keyboard;
+    ISceneNode*           scene;
+    ParticleSystem*       particleSystem;
+    bool                  resourcesLoaded;
+    Config(IEngine& engine)
+        : engine(engine)
+        , frame(NULL)
+        , viewport(NULL)
+        , viewingvolume(NULL)
+        , camera(NULL)
+        , frustum(NULL)
+        , renderer(NULL)
+        , mouse(NULL)
+        , keyboard(NULL)
+        , scene(NULL)
+        , particleSystem(NULL)
+        , resourcesLoaded(false)
+    {}
+};
+
+// Forward declaration of the setup methods
+void SetupResources(Config&);
+void SetupDisplay(Config&);
+void SetupParticleSystem(Config&);
+void SetupScene(Config&);
+void SetupRendering(Config&);
+void SetupDevices(Config&);
+void SetupDebugging(Config&);
+
+int main(int argc, char** argv) {
     // Setup logging facilities.
     Logger::AddLogger(new StreamLogger(&std::cout));
 
-    // Start the engine.
-    IGameEngine& engine = GameEngine::Instance();
-    engine.Start(new GameFactory());
+    // Print usage info.
+//     logger.info << "========= Running The OpenEngine Racer Project =========" << logger.end;
+//     logger.info << "This project is a simple testing project for OpenEngine." << logger.end;
+//     logger.info << "It uses an old physics system and is not very stable." << logger.end;
+//     logger.info << logger.end;
+//     logger.info << "Vehicle controls:" << logger.end;
+//     logger.info << "  drive forwards:  up-arrow" << logger.end;
+//     logger.info << "  drive backwards: down-arrow" << logger.end;
+//     logger.info << "  turn left:       left-arrow" << logger.end;
+//     logger.info << "  turn right:      right-arrow" << logger.end;
+//     logger.info << logger.end;
+//     logger.info << "Camera controls:" << logger.end;
+//     logger.info << "  move forwards:   w" << logger.end;
+//     logger.info << "  move backwards:  s" << logger.end;
+//     logger.info << "  move left:       a" << logger.end;
+//     logger.info << "  move right:      d" << logger.end;
+//     logger.info << "  rotate:          mouse" << logger.end;
+//     logger.info << logger.end;
+
+    // Create an engine and config object
+    Engine* engine = new Engine();
+    Config config(*engine);
+
+    // Setup the engine
+    SetupResources(config);
+    SetupDisplay(config);
+    SetupParticleSystem(config);
+    SetupScene(config);
+    SetupRendering(config);
+    SetupDevices(config);
+    
+    // Possibly add some debugging stuff
+    // SetupDebugging(config);
+
+    // Start up the engine.
+    engine->Start();
 
     // Return when the engine stops.
+    delete engine;
     return EXIT_SUCCESS;
+}
+
+void SetupResources(Config& config) {
+    // set the resources directory
+    // @todo we should check that this path exists
+    string resources = "projects/OEParticleSim/data/";
+    DirectoryManager::AppendPath(resources);
+
+    // load resource plug-ins
+    //ResourceManager<IModelResource>::AddPlugin(new OBJPlugin());
+    ResourceManager<ITextureResource>::AddPlugin(new TGAPlugin());
+
+    config.resourcesLoaded = true;
+}
+
+void SetupDisplay(Config& config) {
+    if (config.frame         != NULL ||
+        config.viewingvolume != NULL ||
+        config.camera        != NULL ||
+        config.frustum       != NULL ||
+        config.viewport      != NULL)
+        throw Exception("Setup display dependencies are not satisfied.");
+
+    config.frame         = new SDLFrame(800, 600, 32);
+    config.viewingvolume = new ViewingVolume();
+    config.camera        = new Camera( *config.viewingvolume );
+    config.frustum       = new Frustum(*config.camera, 20, 3000);
+    config.viewport      = new Viewport(*config.frame);
+    config.viewport->SetViewingVolume(config.frustum);
+
+    config.engine.InitializeEvent().Attach(*config.frame);
+    config.engine.ProcessEvent().Attach(*config.frame);
+    config.engine.DeinitializeEvent().Attach(*config.frame);
+}
+
+void SetupRendering(Config& config) {
+    if (config.viewport == NULL ||
+        config.renderer != NULL ||
+        config.scene == NULL)
+        throw Exception("Setup renderer dependencies are not satisfied.");
+
+    // Create a renderer
+    config.renderer = new Renderer();
+
+    // Setup a rendering view
+    RenderingView* rv = new RenderingView(*config.viewport);
+    config.renderer->ProcessEvent().Attach(*rv);
+
+    // Add rendering initialization tasks
+    TextureLoader* tl = new TextureLoader();
+    config.renderer->InitializeEvent().Attach(*tl);
+
+    // Supply the scene to the renderer
+    config.renderer->SetSceneRoot(config.scene);
+
+    config.engine.InitializeEvent().Attach(*config.renderer);
+    config.engine.ProcessEvent().Attach(*config.renderer);
+    config.engine.DeinitializeEvent().Attach(*config.renderer);
+}
+
+void SetupDevices(Config& config) {
+    if (config.mouse    != NULL ||
+        config.keyboard != NULL ||
+        config.camera  == NULL) 
+        throw Exception("Setup keyboard dependencies are not satisfied.");
+
+    // Create the mouse and keyboard input modules
+    config.keyboard = new SDLInput();
+    config.mouse = (IMouse*)config.keyboard;
+
+    // Bind the quit handler
+    QuitHandler* quit_h = new QuitHandler(config.engine);
+    config.keyboard->KeyEvent().Attach(*quit_h);
+
+//     // Register movement handler to be able to move the camera
+    MoveHandler* move_h = new MoveHandler(*config.camera, *config.mouse);
+    config.keyboard->KeyEvent().Attach(*move_h);
+
+    // Bind to the engine for processing time
+    config.engine.InitializeEvent().Attach(*config.mouse);
+    config.engine.ProcessEvent().Attach(*config.mouse);
+    config.engine.DeinitializeEvent().Attach(*config.mouse);
+
+    config.engine.InitializeEvent().Attach(*move_h);
+    config.engine.ProcessEvent().Attach(*move_h);
+    config.engine.DeinitializeEvent().Attach(*move_h);
+}
+
+void SetupParticleSystem (Config& config) {
+    
+    config.particleSystem = new ParticleSystem();
+    
+    // Add to engine for processing time (with its timer)
+    ParticleSystemTimer* pstimer = new ParticleSystemTimer(*config.particleSystem);
+    config.engine.InitializeEvent().Attach(*config.particleSystem);
+    config.engine.ProcessEvent().Attach(*pstimer);
+    config.engine.DeinitializeEvent().Attach(*config.particleSystem);
+
+}
+
+void SetupScene(Config& config) {
+    if (config.scene  != NULL ||
+        config.particleSystem == NULL ||
+        config.resourcesLoaded == false)
+        throw Exception("Setup scene dependencies are not satisfied.");
+
+
+    config.camera->SetPosition(Vector<3,float>(100,0,100));
+    config.camera->LookAt(Vector<3,float>(0.0,0.0,0.0));
+
+    // Create scene nodes
+    config.scene = new SceneNode();
+
+
+    FireNode* fireNode = new FireNode(config.particleSystem);
+    config.scene->AddNode( fireNode );
+    config.particleSystem->ProcessEvent().Attach(*fireNode);
+}
+
+void SetupDebugging(Config& config) {
+
+    // Visualization of the frustum
+    if (config.frustum != NULL) {
+        config.frustum->VisualizeClipping(true);
+        config.scene->AddNode(config.frustum->GetFrustumNode());
+    }
+
+    // Add Statistics module
+    config.engine.ProcessEvent().Attach(*(new OpenEngine::Utils::Statistics(1000)));
 }
